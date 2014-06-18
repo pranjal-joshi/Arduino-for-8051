d949e59849b36d25482a101811bb1eaea4aac6e9
Pranjal Joshi <joshi.pranjal5@gmail.com>
2014-06-18 12:06:40 +0530

    New LCD file added. Bugs removed!

--- Ardu_lcd51.h
+++ Ardu_lcd51.h
@@ -2,76 +2,129 @@
 #include <arduino51.h>
 
 // Function definations :
+ 
+#define LCD_EN 0x20
+#define LCD_RS 0x10
 
-void lcd_init(void);
-
-void lcd_begin(unsigned char rs, unsigned char en, unsigned char d4, unsigned char d5, unsigned char d6, unsigned char d7)
+unsigned char COUNTER;
+ 
+void lcd_cmd(char cmd)
 {
-	rs_pin = rs;
-	en_pin = en;
-	d4_pin = d4;
-	d5_pin = d5;
-	d6_pin = d6;
-	d7_pin = d7;
-	lcd_init();
+lcd_port = ((cmd >> 4) & 0x0F)|LCD_EN;
+lcd_port = ((cmd >> 4) & 0x0F);
+ 
+lcd_port = (cmd & 0x0F)|LCD_EN;
+lcd_port = (cmd & 0x0F);
+ 
+delay(2);
+}
+ 
+void lcd_data(unsigned char dat)
+{
+lcd_port = (((dat >> 4) & 0x0F)|LCD_EN|LCD_RS);
+lcd_port = (((dat >> 4) & 0x0F)|LCD_RS);
+lcd_port = ((dat & 0x0F)|LCD_EN|LCD_RS);
+lcd_port = ((dat & 0x0F)|LCD_RS);
+ 
+delay(2);
 }
 
-void lcdWrite(unsigned char val)
+void lcd_reset()
 {
-	unsigned char temp;
-	temp = val;
-	val = ((val >> 4) & 0x0F);
-	d4_pin_val = (val >> 0) & 0x01;
-	d5_pin_val = (val >> 1) & 0x01;
-	d6_pin_val = (val >> 2) & 0x01;
-	d7_pin_val = (val >> 3) & 0x01;
-	digitalWrite(d4_pin,d4_pin_val);
-	digitalWrite(d5_pin,d5_pin_val);
-	digitalWrite(d6_pin,d6_pin_val);
-	digitalWrite(d7_pin,d7_pin_val);
-	digitalWrite(en_pin,HIGH);
-	delay(2);
-	digitalWrite(en_pin,LOW);
-	val = temp;
-	val &= 0x0F;
-	d4_pin_val = (val >> 0) & 0x01;
-	d5_pin_val = (val >> 1) & 0x01;
-	d6_pin_val = (val >> 2) & 0x01;
-	d7_pin_val = (val >> 3) & 0x01;	
-	digitalWrite(d4_pin,d4_pin_val);
-	digitalWrite(d5_pin,d5_pin_val);
-	digitalWrite(d6_pin,d6_pin_val);
-	digitalWrite(d7_pin,d7_pin_val);
-	digitalWrite(en_pin,HIGH);
-	delay(2);
-	digitalWrite(en_pin,LOW);
+lcd_port = 0xFF;
+delay(20);
+lcd_port = 0x03+LCD_EN;
+lcd_port = 0x03;
+delay(10);
+lcd_port = 0x03+LCD_EN;
+lcd_port = 0x03;
+delay(1);
+lcd_port = 0x03+LCD_EN;
+lcd_port = 0x03;
+delay(1);
+lcd_port = 0x02+LCD_EN;
+lcd_port = 0x02;
+delay(1);
 }
-
-void lcdcmd(unsigned char cmd)
+ 
+void lcd_begin()
 {
-	digitalWrite(rs_pin,LOW);
-	lcdWrite(cmd);
-	delay(1);
-}
-
-void lcddata(unsigned char LCDdata)
-{
-	digitalWrite(rs_pin,HIGH);
-	lcdWrite(LCDdata);
-	delay(1);
+lcd_reset(); // Call LCD reset
+lcd_cmd(0x28); // 4-bit mode - 2 line - 5x7 font.
+lcd_cmd(0x0C); // Display no cursor - no blink.
+lcd_cmd(0x06); // Automatic Increment - No Display shift.
+lcd_cmd(0x80); // Address DDRAM with 0 offset 80h.
 }
 
 void lcd_print(unsigned char *str)
 {
 	unsigned int x;
 	for(x=0;str[x]!=0;x++)
-		lcddata(str[x]);
+		lcd_data(str[x]);
 }
 
-void lcd_init()
+void lcd_println(unsigned char *str)
 {
-	lcdcmd(0x28);
-	lcdcmd(0x0C);
-	lcdcmd(0x06);
-	lcdcmd(0x80);
+	unsigned int x;
+	for(x=0;str[x]!=0;x++)
+		lcd_data(str[x]);
+	lcd_cmd(0xC0);
 }
+
+void lcd_clear()
+{
+	lcd_cmd(0x01);
+}
+
+void lcd_cursorOn()
+{
+	lcd_cmd(0x0E);
+}
+
+void lcd_cursorOff()
+{
+	lcd_cmd(0x0C);
+}
+
+void lcd_cursorBlink()
+{
+	lcd_cmd(0x0F);
+}
+
+void lcd_displayOff()
+{
+	lcd_cmd(0x08);
+}
+
+void lcd_displayOn()
+{
+	lcd_cmd(0x0C);
+}
+
+void lcd_shiftLeft(unsigned char howMany, unsigned int DelayMS)
+{
+	for(COUNTER=0;COUNTER<howMany;COUNTER++)
+	{
+		lcd_cmd(0x18);
+		delay(DelayMS);
+	}
+}
+
+void lcd_shiftRight(unsigned char howMany, unsigned int DelayMS)
+{
+	for(COUNTER=0;COUNTER<howMany;COUNTER++)
+	{
+		lcd_cmd(0x1C);
+		delay(DelayMS);
+	}
+}
+
+void lcd_setCursor(unsigned char row, unsigned char coloumn)
+{
+	if(row == 1)
+		COUNTER = 0x80;
+	else if(row == 2)
+		COUNTER = 0xC0;
+	lcd_cmd(COUNTER + coloumn - 1);
+}
+
